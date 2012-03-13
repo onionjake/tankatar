9ac471bc3fb4e6324c10542651d5650c5a1f2a8a
onionjake <jake@willoughbys.net>
2012-03-13 10:27:15 -0600

    Add keyboard, mouse listeners.

--- core/src/main/java/com/github/onionjake/tankatar/core/Tankatar.java
+++ core/src/main/java/com/github/onionjake/tankatar/core/Tankatar.java
@@ -5,14 +5,41 @@ import static playn.core.PlayN.*;
 import playn.core.Game;
 import playn.core.Image;
 import playn.core.ImageLayer;
+import playn.core.Keyboard;
+import playn.core.Pointer;
+import playn.core.Sound;
 
-public class Tankatar implements Game {
+public class Tankatar implements Game, Keyboard.Listener {
+
+  private Sound ding;
+  private float touchVectorX, touchVectorY;
+
   @Override
   public void init() {
     // create and add background image layer
     Image bgImage = assets().getImage("images/bg.png");
     ImageLayer bgLayer = graphics().createImageLayer(bgImage);
     graphics().rootLayer().add(bgLayer);
+
+    keyboard().setListener(this);
+    pointer().setListener(new Pointer.Listener() {
+      @Override
+      public void onPointerEnd(Pointer.Event event) {
+        touchVectorX = touchVectorY = 0;
+      }
+      @Override
+      public void onPointerDrag(Pointer.Event event) {
+        touchMove(event.x(), event.y());
+      }
+      @Override
+      public void onPointerStart(Pointer.Event event) {
+        touchMove(event.x(), event.y());
+      }
+    });
+
+    ding = assets().getSound("ding");
+
+    ding.play();
   }
 
   @Override
@@ -22,6 +49,26 @@ public class Tankatar implements Game {
 
   @Override
   public void update(float delta) {
+  }
+
+  @Override
+  public void onKeyDown(Keyboard.Event event) {
+  }
+
+  @Override
+  public void onKeyTyped(Keyboard.TypedEvent event) {
+  }
+
+  @Override
+  public void onKeyUp(Keyboard.Event event) {
+  }
+
+  private void touchMove(float x, float y) {
+    float cx = graphics().screenWidth() / 2;
+    float cy = graphics().screenHeight() / 2;
+
+    touchVectorX = (x - cx) * 1.0f / cx;
+    touchVectorY = (y - cy) * 1.0f / cy;
   }
 
   @Override
